dcc156513665f73602580d3d1609c5a0f614fc53
Anton Malinskiy <anton@malinskiy.com>
2021-09-01 22:58:41 +1000

    Update marathon.rb

--- Formula/marathon.rb
+++ Formula/marathon.rb
@@ -1,8 +1,8 @@
 class Marathon < Formula
   desc "Cross-platform test runner written for Android and iOS projects"
   homepage "https://marathonlabs.github.io/marathon/"
-  url "https://github.com/MarathonLabs/marathon/releases/download/0.6.3/marathon-0.6.3.zip"
-  sha256 "6bae76a502693f3b02673ddcd3feb5a627c106e803e77b450f4cbafbdfc24981"
+  url "https://github.com/MarathonLabs/marathon/releases/download/0.6.4/marathon-0.6.4.zip"
+  sha256 "2502ffb2e4de1f56f3e09df32a151c64fba1c4cdc662993aec5b35b7fdb72a34"
 
   bottle :unneeded
 
