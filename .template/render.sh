e25e7a7052860b83809deea1c528ec95f1475a9f
Anton Malinskiy <anton@malinskiy.com>
2021-09-13 12:36:59 +1000

    feat(ci): specify output file

--- .template/render.sh
+++ .template/render.sh
@@ -2,6 +2,9 @@
 
 set -xe
 
+INPUT="$1"
+OUTPUT="$2"
+
 echo "Rendering for version $VERSION, expected sha is $SHA"
 URL="https://github.com/MarathonLabs/marathon/releases/download/${VERSION}/marathon-${VERSION}.zip"
 SHA256="$(curl -L $URL | sha256sum | cut -d " " -f 1)"
@@ -9,7 +12,7 @@ echo "Calculated sha256 $SHA256"
 
 if [ "$SHA" == "$SHA256" ]; then
   echo "SHA256 matches"
-  sed "s/\${VERSION}/$VERSION/g" "$1" | sed "s/\${SHA}/$SHA/g"
+  sed "s/\${VERSION}/$VERSION/g" "$INPUT" | sed "s/\${SHA}/$SHA/g" > "$OUTPUT"
 else
   echo "SHA $SHA256 does not match expected $SHA"
   exit 1
