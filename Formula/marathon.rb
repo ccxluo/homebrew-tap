c9ce1c3cf95e78813e41bec00b53c3acf341451b
Anton Malinskiy <anton@malinskiy.com>
2022-09-16 19:51:22 +1000

    Update marathon.rb

--- Formula/marathon.rb
+++ Formula/marathon.rb
@@ -1,8 +1,8 @@
 class Marathon < Formula
   desc "Cross-platform test runner written for Android and iOS projects"
   homepage "https://marathonlabs.github.io/marathon/"
-  url "https://github.com/MarathonLabs/marathon/releases/download/0.7.5/marathon-0.7.5.zip"
-  sha256 "a1d35163bd493601f34113ecfa5440a9b53eae22a005959767aa02a198254750"
+  url "https://github.com/MarathonLabs/marathon/releases/download/0.7.6/marathon-0.7.6.zip"
+  sha256 "5157d57c978c46931ca0c8bf5efd43ea17ca35b622ef4ce41113268a8f7dcdde"
 
   depends_on "openjdk@11"
 
@@ -13,6 +13,6 @@ class Marathon < Formula
   end
 
   test do
-    assert_match "marathon v0.7.5: unrecognized option '--version'", shell_output("#{bin}/marathon --version")
+    assert_match "marathon v0.7.6: unrecognized option '--version'", shell_output("#{bin}/marathon --version")
   end
 end
