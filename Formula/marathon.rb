05771e9af89f9aaec99cab987e155c7f45200903
Anton Malinskiy <anton@malinskiy.com>
2024-01-27 00:13:52 +1000

    Update marathon.rb to 0.9.1

--- Formula/marathon.rb
+++ Formula/marathon.rb
@@ -1,8 +1,8 @@
 class Marathon < Formula
   desc "Cross-platform test runner written for Android and iOS projects"
   homepage "https://marathonlabs.github.io/marathon/"
-  url "https://github.com/MarathonLabs/marathon/releases/download/0.9.0/marathon-0.9.0.zip"
-  sha256 "531548ed15375e33e92690eccdb71d65937278dc13f5cfcac1ca6615e99d2b0a"
+  url "https://github.com/MarathonLabs/marathon/releases/download/0.9.1/marathon-0.9.1.zip"
+  sha256 "060792d9483695f677cb1c73e35a808555281babfb034a9c5b2c3fc98a19e1da"
 
   depends_on "openjdk@11"
 
